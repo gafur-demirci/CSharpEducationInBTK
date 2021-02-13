99282c42b179981b688112936d2b26af3a05e3c9
gafur-demirci <73399810+gafur-demirci@users.noreply.github.com>
2021-02-13 18:27:53 +0300

    BTK C# Kursu /w Engin Demiroğ

---
+++ Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer customer = new Customer();
+            customer.Id = 1;
+            customer.FirstName = "Merve";
+            customer.LastName = "Olçun";
+            customer.City = "Eskişehir";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                FirstName = "Eda Nur",
+                LastName = "Kara",
+                City = "Trabzon"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+        }
+    }
+}
